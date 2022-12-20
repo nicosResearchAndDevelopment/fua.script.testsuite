cf39540455812733fabbec3cc141ac4555802116
Simon Petrac <pfoerdie@gmail.com>
2022-12-20 11:38:35 +0100

    wip: TestsuiteAgent

--- ec/net/test/ts.ec.net.next.test.js
+++ ec/net/test/ts.ec.net.next.test.js
@@ -1,35 +1,36 @@
 const
     {describe, test, before, after} = require('mocha'),
     expect                          = require('expect'),
+    util                            = require('../src/ts.ec.net.util.js'),
     testing                         = require('@nrd/fua.module.testing'),
-    util                            = require('../src/ts.ec.net.util.js');
+    TestsuiteAgent                  = require('../../../src/code/agent.testsuite.next.js');
 
 describe('ts.ec.net.ping', function () {
 
     this.timeout('10s');
 
-    /** @type {fua.module.testing.TestingConsumer} */
     let agent = null;
 
     before('init', async function () {
-        agent = await testing.Consumer.from({
-            connectType:    'http',
-            connectOptions: {
+        agent = await TestsuiteAgent.create({
+            connect: {
+                type:               'http',
                 url:                'https://testbed.nicos-rd.com:8080/testing',
                 rejectUnauthorized: false
-            }
-        }).init({
-            session:    [__dirname, '../../../session/session.json'],
-            properties: {
-                //suite:    this.test.parent.titlePath().join('.'),
-                date:     util.localDate(),
-                operator: 'test@nicos-ag.com'
+            },
+            session: {
+                file:       [__dirname, '../../../session/session.json'],
+                properties: {
+                    //suite:    this.test.parent.titlePath().join('.'),
+                    date:     util.localDate(),
+                    operator: 'test@nicos-ag.com'
+                }
             }
         });
     }); // before('init')
 
     after('exit', async function () {
-        await agent.exit();
+        await agent.close();
     }); // after
 
     test('urn:tb:ec:net:tc:reachable', async function () {
@@ -37,7 +38,7 @@ describe('ts.ec.net.ping', function () {
             ecosystem: 'urn:tb:ec:net',
             testCase:  'urn:tb:ec:net:tc:reachable',
             param:     {
-                host: agent.property('host')
+                host: agent.testing.property('host')
             }
         });
         console.log(token.serialize());
---
+++ src/code/agent.testsuite.next.js
@@ -0,0 +1,53 @@
+const
+    util        = require('./util.testsuite.js'),
+    ServerAgent = require('@nrd/fua.agent.server'),
+    testing     = require('@nrd/fua.module.testing');
+
+class TestsuiteAgent extends ServerAgent {
+
+    static id = 'http://www.nicos-rd.com/fua/testbed#TestsuiteAgent';
+
+    #testing = null;
+
+    async initialize(options = {}) {
+
+        await super.initialize(options);
+
+        if (options.connect && options.session) {
+            const {type: connectType, ...connectOptions} = options.connect || {};
+            util.assert(util.isString(connectType), 'expected connectType to be a string');
+
+            const {file: session, properties} = options.session || {};
+            util.assert(util.isString(session) || util.isArray(session), 'expected session to be a string or string array');
+            util.assert(util.isObject(properties), 'expected properties to be an object');
+
+            this.#testing = await testing.Consumer.from({
+                '@id': this.uri,
+                connectType,
+                connectOptions
+            }).init({
+                session,
+                properties
+            });
+        }
+
+        return this;
+    } // TestsuiteAgent#initialize
+
+    get testing() {
+        return this.#testing;
+    }
+
+    async close() {
+        if (this.testing) await this.testing.exit();
+        if (this.server) await super.close();
+    } // TestsuiteAgent#close
+
+    async test(tokenArgs = {}) {
+        util.assert(this.testing, 'expected testing to be defined');
+        return await this.testing.test(tokenArgs);
+    } // TestsuiteAgent#test
+
+} // TestsuiteAgent
+
+module.exports = TestsuiteAgent;
