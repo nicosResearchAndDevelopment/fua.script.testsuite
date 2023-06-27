c053e9dc514d2c2353b1a8dea2e8619b770a6041
Simon Petrac <pfoerdie@gmail.com>
2023-06-27 11:29:15 +0200

    improved datetime test and added config for insecure local connection

--- src/ts.config.js
+++ src/ts.config.js
@@ -1,6 +1,7 @@
 const
     config     = exports,
     util       = require('./ts.util.js'),
+    https      = require('https'),
     subprocess = require('@nrd/fua.module.subprocess'),
     args       = subprocess.parseArgv(),
     env        = process.env;
@@ -9,13 +10,16 @@ config.id = args.id || env.TESTSUITE_ID || null;
 
 config.connect = {
     type:    'http',
-    url:     args.url || env.TESTBED_URL || 'https://tb.nicos-rd.com/testing',
+    url:     args.url || env.TESTING_URL || 'https://tb.nicos-rd.com/testing',
     headers: {
         'Authorization': util.basicAuth(
             args.username || env.TESTSUITE_USERNAME || 'testsuite',
             args.password || env.TESTSUITE_PASSWORD || 'testsuite'
         )
-    }
+    },
+    agent:   new https.Agent({
+        rejectUnauthorized: !(args.insecure || env.INSECURE)
+    })
 };
 
 config.session = {
--- tests/ts.ec.dev.js
+++ tests/ts.ec.dev.js
@@ -7,13 +7,34 @@ describe('ts.ec.dev', function () {
     this.timeout('10s');
 
     test('datetime', async function () {
-        const token = await this.ts.test({
-            ecosystem:  'urn:tb:ec:dev',
-            testMethod: 'urn:tb:ec:dev:tm:datetime'
-        });
-        console.log(token.serialize());
+        const
+            tolerance    = 200,
+            startDate    = new Date(),
+            token        = await this.ts.test({
+                ecosystem:  'urn:tb:ec:dev',
+                testMethod: 'urn:tb:ec:dev:tm:datetime'
+            }),
+            endDate      = new Date(),
+            tokenDate    = new Date(token.result.datetime),
+            timeBefore   = tokenDate - startDate,
+            timeAfter    = endDate - tokenDate,
+            dateToString = (date) => date.getHours().toString().padStart(2, '0') + ':'
+                + date.getMinutes().toString().padStart(2, '0') + ':'
+                + date.getSeconds().toString().padStart(2, '0') + '.'
+                + date.getMilliseconds().toString().padStart(3, '0'),
+            timeToString = (time) => (time < 0 ? '-' : '+')
+                + dateToString(new Date(
+                    0, 0, 0,
+                    Math.floor(Math.abs(time) / 3600000),
+                    Math.floor(Math.abs(time) / 60000) % 60,
+                    Math.floor(Math.abs(time) / 1000) % 60,
+                    Math.abs(time) % 1000
+                )).replace(/00:/g, '').replace(/^0+(?=\d)/, '');
+
         expect(token.result.datetime).toBeTruthy();
-        console.log('date:', new Date(token.result.datetime));
+        console.log(`datetime: ${dateToString(startDate)} [${timeToString(-timeBefore)}] < ${dateToString(tokenDate)} < [${timeToString(timeAfter)}] ${dateToString(endDate)}`);
+        expect(timeBefore).toBeGreaterThan(-tolerance);
+        expect(timeAfter).toBeGreaterThan(-tolerance);
     });
 
 });
