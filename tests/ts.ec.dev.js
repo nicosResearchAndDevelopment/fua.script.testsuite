6d4c7b0043867d46073eee0507c3c5c2cc83f6fe
Simon Petrac <pfoerdie@gmail.com>
2023-06-27 11:54:28 +0200

    wip

--- tests/ts.ec.dev.js
+++ tests/ts.ec.dev.js
@@ -8,33 +8,34 @@ describe('ts.ec.dev', function () {
 
     test('datetime', async function () {
         const
-            tolerance    = 200,
-            startDate    = new Date(),
-            token        = await this.ts.test({
+            startDate      = new Date(),
+            token          = await this.ts.test({
                 ecosystem:  'urn:tb:ec:dev',
                 testMethod: 'urn:tb:ec:dev:tm:datetime'
             }),
-            endDate      = new Date(),
-            tokenDate    = new Date(token.result.datetime),
-            timeBefore   = tokenDate - startDate,
-            timeAfter    = endDate - tokenDate,
-            dateToString = (date) => date.getHours().toString().padStart(2, '0') + ':'
+            endDate        = new Date(),
+            tokenDate      = new Date(token.result.datetime),
+            timeBefore     = tokenDate.getTime() - startDate.getTime(),
+            timeAfter      = endDate.getTime() - tokenDate.getTime(),
+            dateToString   = (date) => date.getHours().toString().padStart(2, '0') + ':'
                 + date.getMinutes().toString().padStart(2, '0') + ':'
                 + date.getSeconds().toString().padStart(2, '0') + '.'
                 + date.getMilliseconds().toString().padStart(3, '0'),
-            timeToString = (time) => (time < 0 ? '-' : '+')
-                + dateToString(new Date(
-                    0, 0, 0,
+            timeToString   = (time) => (time < 0 ? '-' : '+')
+                + dateToString(new Date(0, 0, 0,
                     Math.floor(Math.abs(time) / 3600000),
                     Math.floor(Math.abs(time) / 60000) % 60,
                     Math.floor(Math.abs(time) / 1000) % 60,
                     Math.abs(time) % 1000
-                )).replace(/00:/g, '').replace(/^0+(?=\d)/, '');
+                )).replace(/00:/g, '').replace(/^0+(?=\d)/, ''),
+            tolerance      = 500,
+            toleratedStart = new Date(startDate.getTime() - tolerance),
+            toleratedEnd   = new Date(endDate.getTime() + tolerance);
 
         expect(token.result.datetime).toBeTruthy();
-        console.log(`datetime: ${dateToString(startDate)} [${timeToString(-timeBefore)}] < ${dateToString(tokenDate)} < [${timeToString(timeAfter)}] ${dateToString(endDate)}`);
-        expect(-timeBefore).toBeLessThan(tolerance);
-        expect(-timeAfter).toBeLessThan(tolerance);
+        console.log(`datetime: ${dateToString(startDate)} < [${timeToString(timeBefore)}] ${dateToString(tokenDate)} < [${timeToString(timeAfter)}] ${dateToString(endDate)}`);
+        expect(tokenDate.getTime()).toBeGreaterThan(toleratedStart.getTime());
+        expect(tokenDate.getTime()).toBeLessThan(toleratedEnd.getTime());
     });
 
 });
